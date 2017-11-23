64f0eb048257397052ab7310c8628b91e73079c2
BaptisteBR <baptiste.briot@gmail.com>
2017-11-23 17:54:16 +0100

    Remove python folder

--- neuronal_network.py
+++ neuronal_network.py
@@ -7,11 +7,12 @@
 """
 """
 
+from __future__ import print_function
 import tensorflow as tf
 
 if __name__ == "__main__":
     
-    # Create nodes
+    # Create nodes 1 & 2
     node1 = tf.constant(3.0, dtype=tf.float32)
     node2 = tf.constant(4.0)
     
@@ -21,3 +22,10 @@ if __name__ == "__main__":
     sess = tf.Session()
     
     print(sess.run([node1, node2]))
+    
+    # Create node 3
+    node3 = tf.add(node1, node2)
+    
+    print("node3:", node3)
+    
+    print("sess.run(node3):", sess.run(node3))
