670323429b61f3cb75fa32e4eca19e82250c0642
BaptisteBR <baptiste.briot@gmail.com>
2017-11-23 18:29:51 +0100

    Add example and complete get_started

--- neuronal_network.py
+++ neuronal_network.py
@@ -2,9 +2,8 @@
 # <baptiste.briot--ribeyre@alumni.univ-avignon.fr>
 
 """
+    https://www.tensorflow.org/get_started/get_started
 """
-
-
 
 from __future__ import print_function
 import tensorflow as tf
@@ -28,3 +27,56 @@ if __name__ == "__main__":
     print("node3:", node3)
     
     print("sess.run(node3):", sess.run(node3))
+    
+    # Create placeholders a & b
+    a = tf.placeholder(tf.float32)
+    b = tf.placeholder(tf.float32)
+    
+    adder_node = a + b # + provide a shorcut for tf.add(a, b)
+    
+    print(sess.run(adder_node, {a: 3, b: 4.5}))
+    print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+    
+    # Add another operation
+    add_and_triple = adder_node * 3
+    
+    print(sess.run(add_and_triple, {a: 3, b: 4.5}))
+    
+    # Create variables
+    W = tf.Variable([.3], dtype=tf.float32)
+    b = tf.Variable([-.3], dtype=tf.float32)
+    #
+    x = tf.placeholder(tf.float32)
+    
+    linear_model = W*x + b
+    
+    # Initialize global variables
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    
+    print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+    
+    # Create loss function
+    y = tf.placeholder(tf.float32)
+    squared_deltas = tf.square(linear_model - y)
+    loss = tf.reduce_sum(squared_deltas)
+    
+    print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+    
+    # Reassigning W & b values
+    fixW = tf.assign(W, [-1.])
+    fixb = tf.assign(b, [1.])
+    
+    sess.run([fixW, fixb])
+    
+    print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]}))
+    
+    # Create optimizer
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = optimizer.minimize(loss)
+    
+    sess.run(init) # reset values to incorrect defaults
+    for i in range(1000):
+        sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+    
+    print(sess.run([W, b]))
